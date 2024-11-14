54c449675e315899734288b172c2bb96ce328bd1
Kim <kimngu01@easv365.dk>
2024-11-14 10:13:21 +0100

    Github

--- src/main/java/dk/easv/demoproject01/HelloController.java
+++ src/main/java/dk/easv/demoproject01/HelloController.java
@@ -11,4 +11,12 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    public void Github(){
+
+
+    }
+
+
+
 }
